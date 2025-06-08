ff97c8d62f8c702bb133650af1493f7dc6f24361
Emanuel <nuill.exe@gmail.com>
2025-06-07 22:15:57 -0300

    Apply Binary Search algorithm using a while loop

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,49 @@
+import java.util.Arrays;
 import java.util.Scanner;
+/*
+Solicitar 20 nomes quaisquer que serão armazenados em uma matriz do tipo vetor. Ordene
+este vetor em ordem alfabética. Exiba na tela os 20 nomes na ordem. Em seguida solicite um
+nome para pesquisa. Caso o nome fornecido exista no vetor, informar a sua localização (seu
+índice).
+*/
 
 public class Main {
+    public static int searchPositionName(String[]array,String name){
+        int left=0, right=array.length-1,mid;
+
+        while(left<=right){
+            mid=(right+left)/2;
+            //System.out.printf("L:%d\tR:%d\tmid:%d\n",left,right,mid);
+            // This print statement displays the values of the variables left, right, and mid, which are essential for
+            // understanding the algorithm's behavior.
+            int comparision=array[mid].compareToIgnoreCase(name);
+            if(comparision==0){
+                return mid;
+            }else if( comparision<0){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        String[] name= new String[20];
+        String[] names= new String[10];
         String searchName;
-        boolean findName=false;
-        int findNameIndex=0;
 
         System.out.println("Enter names for will be storage in memory");
-        for(int i=0; i<20; i++){
-            name[i]= sc.nextLine();
+        for(int i=0; i<10; i++){
+            names[i]= sc.nextLine();
         }
-        String sketch;
+        //Sort the array names in alphabetic order
+        Arrays.sort(names,String.CASE_INSENSITIVE_ORDER);
 
-        for(int i=0; i<20; i++){
-            for(int j=0; j<20; j++){
-                if(name[i].codePointAt(0)< name[j].codePointAt(0)){
-                    sketch=name[j];
-                    name[j]=name[i];
-                    name[i]=sketch;
-                }
-            }
-        }
         System.out.println("Search name to be research in array");
         searchName=sc.nextLine();
-        for(int i=0; i<20; i++){
-            if(name[i].equals(searchName)){
-                findName=true;
-                findNameIndex=i;
-            }
-        }
-        if(findName){
-            System.out.println("This name wasn find");
+        int findNameIndex=searchPositionName(names,searchName);
+        if(findNameIndex!=-1){
+            System.out.println("This name was find");
             System.out.printf("Name: %s\t Index: %d", searchName,findNameIndex);
         }else{
             System.out.println("This name wasn't find");

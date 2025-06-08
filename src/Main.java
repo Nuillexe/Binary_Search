26233aff4e38c000a35dfdfb54cac8b057e5c475
Emanuel <nuill.exe@gmail.com>
2025-06-07 22:46:42 -0300

    Implement Binary Search algorithm using recursion

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,36 @@ nome para pesquisa. Caso o nome fornecido exista no vetor, informar a sua locali
 */
 
 public class Main {
-    public static int searchPositionName(String[]array,String name){
-        int left=0, right=array.length-1,mid;
+    public static int searchPositionName(String[]array,String name, int left, int right){
+        int mid;
+        mid=(right+left)/2;
+        System.out.printf("L:%d\tR:%d\tmid:%d\n",left,right,mid);
+        // This print statement displays the values of the variables left, right, and mid, which are essential for
+        // understanding the algorithm's behavior.
 
-        while(left<=right){
-            mid=(right+left)/2;
-            //System.out.printf("L:%d\tR:%d\tmid:%d\n",left,right,mid);
-            // This print statement displays the values of the variables left, right, and mid, which are essential for
-            // understanding the algorithm's behavior.
-            int comparision=array[mid].compareToIgnoreCase(name);
-            if(comparision==0){
-                return mid;
-            }else if( comparision<0){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
+        int comparision=array[mid].compareToIgnoreCase(name);
+        if(left>right){//When 'left' become greater than 'right', the seached name does not exist in the array
+            return -1;
         }
-        return -1;
+        if(comparision==0){
+            return mid;
+        }else if( comparision<0){
+            left=mid+1;
+            return searchPositionName(array,name,left,right);
+        }else{
+            right = mid - 1;
+            return searchPositionName(array, name, left, right);
+        }
+
     }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        String[] names= new String[10];
+        String[] names= new String[20];
         String searchName;
 
         System.out.println("Enter names for will be storage in memory");
-        for(int i=0; i<10; i++){
+        for(int i=0; i<20; i++){
             names[i]= sc.nextLine();
         }
         //Sort the array names in alphabetic order
@@ -41,7 +45,7 @@ public class Main {
 
         System.out.println("Search name to be research in array");
         searchName=sc.nextLine();
-        int findNameIndex=searchPositionName(names,searchName);
+        int findNameIndex=searchPositionName(names,searchName,0,names.length-1);
         if(findNameIndex!=-1){
             System.out.println("This name was find");
             System.out.printf("Name: %s\t Index: %d", searchName,findNameIndex);
